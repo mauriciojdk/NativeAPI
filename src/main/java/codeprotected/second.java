b87807be704f210c24391d06680d26bf4caee843
mauricio <mauriciojdk@gmail.com>
2021-12-14 21:28:50 -0300

    test

--- src/main/java/codeprotected/second.java
+++ src/main/java/codeprotected/second.java
@@ -3,6 +3,6 @@ package codeprotected;
 public class second {
 
     public second(){
-        //new cprotected().sobrenome //Consegue usar atributo protected por esta no mesmo pacote
+       new cprotected().sobrenome //Consegue usar atributo protected por esta no mesmo pacote
     }
 }
